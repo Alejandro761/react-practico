85bac19702a299ce9a04dd9603f09e72c594eb4e
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-07-19 22:37:04 -0500

    custom hook y mandado a llamar los elementos de la api. 20

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import {ProductItem} from '../components/ProductItem';
+import React, {useEffect, useState} from 'react';
+import {ProductItem} from '@components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios';
+import { useGetProducts } from '../hooks/useGetProducts';
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+	const products = useGetProducts(API);
+
+	
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem key={product.id} product={product}/>
+				))}
 			</div>
 		</section>
 	);
