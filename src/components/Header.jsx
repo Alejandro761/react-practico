df015791f776a36b3122efa1cf0d8c5fd46f469a
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-07-19 23:49:45 -0500

    condicional para mostrar el numero de productos en el carrito y corregí el custom hook porque no estaba copiando bien los elementos de cart. 23

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import '@styles/Header.scss';
 import { Menu } from "./Menu";
 import menu from '@icons/icon_menu.svg'
 import logo from '@logos/logo_yard_sale.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg';
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const {state: {cart}} = useContext(AppContext)
   
   const handleToggle = () => {
     setToggle(!toggle);
@@ -45,7 +47,7 @@ const Header = () => {
             </li>
             <li className="navbar-shopping-cart">
               <img src={shoppingCart} alt="shopping cart" />
-              <div>2</div>
+              {cart.length > 0 && <div>{cart.length}</div>}
             </li>
           </ul>
         </div>
